554aa8d836f657a0fae10ba004e11903d644f335
Jura Khrapunov <jura.khrapunov@undp.org>
2026-07-14 09:46:49 -0400

    Updated translations handling

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -4,17 +4,14 @@ const config: Config<Record<string, unknown>> = {
 	loaders: [
 		{
 			locale: 'en',
-			key: 'app',
 			loader: async () => (await import('./translations/en.json')).default
 		},
 		{
 			locale: 'ru',
-			key: 'app',
 			loader: async () => (await import('./translations/ru.json')).default
 		},
 		{
 			locale: 'uk',
-			key: 'app',
 			loader: async () => (await import('./translations/uk.json')).default
 		}
 	]
